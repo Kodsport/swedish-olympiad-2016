efee1ea0372edab4c2747dc22c8f1557ecb440ea
Arash Rouhani <miffoljud@gmail.com>
2015-11-24 22:23:21 +0700

    forvirradforelasare: First draft

---
+++ skolkval/forvirradforelasare/input_format_validators/validator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+data = []
+
+for line in sys.stdin:
+    if line.strip() != "":
+        data.append(line.strip())
+
+assert len(data) == 2
+
+N = int(data[0])
+assert 1 <= N <= 9
+
+days = map(int, data[1].split())
+assert len(days) == N
+
+assert all(0 <= day <= 10 for day in days)
+
+sys.exit(42)
