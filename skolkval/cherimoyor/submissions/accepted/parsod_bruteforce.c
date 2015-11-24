2bfe9798d907ea76efbbcc3b73904190eb8b48e6
Par Soderhjelm <par.soderhjelm@teokem.lu.se>
2015-11-24 09:49:06 +0100

    Fixed bug in parsod_bruteforce.c

--- skolkval/cherimoyor/submissions/accepted/parsod_bruteforce.c
+++ skolkval/cherimoyor/submissions/accepted/parsod_bruteforce.c
@@ -13,7 +13,7 @@ int MLX(int nr, int rem1, int rem2) {
         int p,q,eat0,eat1,eat2;
         p=0;
         if(nr==N+2) return 0;
-        for(q=eat2;q<=MIN(10,rem1+rem2+n[nr]);q++) {
+        for(q=rem2;q<=MIN(10,rem1+rem2+n[nr]);q++) {
                 eat2=MIN(q,rem2);
                 eat1=MIN(q-eat2,rem1);
                 eat0=MIN(q-eat1-eat2,n[nr]);
