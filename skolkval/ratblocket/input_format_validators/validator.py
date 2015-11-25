e173131f45ac67acec4ec9c2d876e4722f1fbc6a
Erik Odenman <erik.odenman@gmail.com>
2015-11-25 23:23:00 +0100

    ratblocket: added secret test cases and fixed problem statement a bit

---
+++ skolkval/ratblocket/input_format_validators/validator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys
+
+data = []
+
+for line in sys.stdin:
+    if line.strip() != "":
+        data.append(line.strip())
+
+assert len(data) > 0
+
+line1 = data[0].split()
+assert len(line1) == 2
+
+N,M = map(int, line1)
+assert 1 <= N <= 7
+assert 1 <= M <= 7
+
+assert len(data) == N + 1
+
+a = 0
+b = 0
+
+for line in data[1:]:
+  assert len(line) == M
+  for c in line:
+    if c == "A":
+      a += 1
+    elif c == "B":
+      b += 1
+    else:
+      assert c in ".#c01"
+
+assert a == 1
+assert b == 1
+
+sys.exit(42)
