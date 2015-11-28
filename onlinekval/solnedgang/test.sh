619169e61cfd183b864abc1c2aeebc8944ed4710
Aron Granberg <aron.granberg@gmail.com>
2015-11-28 16:03:25 +0100

    solnedgang: tests script runs and compiles accepted solutions

--- onlinekval/solnedgang/test.sh
+++ onlinekval/solnedgang/test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-for a in tests/*.in; do
-	echo $a
-	python3 input_format_validators/validator.py < $a
-	./a.out < $a | diff -U8 - ${a%.in}.ans
+for acc in submissions/accepted/*.cpp; do
+	set -e
+	g++ -O2 -std=c++11 $acc -o /tmp/solnedgang_ac
+	set +e
+	echo "Testing $acc"
+	for a in tests/*.in; do
+		echo $a
+		python3 input_format_validators/validator.py < $a
+		set -e
+		/tmp/solnedgang_ac < $a | diff -U8 - ${a%.in}.ans
+		set +e
+	done
 done
 
