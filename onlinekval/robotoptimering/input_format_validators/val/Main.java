a5649cd551f4a52d96b8f5f50d33585b3f0538c2
Johan Sannemo <johan.sannemo@gmail.com>
2015-12-02 16:20:18 +0100

    robotoptimering: fix input validator

--- onlinekval/robotoptimering/input_format_validators/val/Main.java
+++ onlinekval/robotoptimering/input_format_validators/val/Main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class InputValidator {
+public class Main {
 
 	public static void main(String... args) throws IOException {
 		Context.readInitialConfig(System.in);
