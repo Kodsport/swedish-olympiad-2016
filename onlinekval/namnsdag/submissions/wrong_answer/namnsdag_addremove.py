2cf8ce71522380e18290a07ea3f1579a4c097924
Johan Sannemo <johan.sannemo@gmail.com>
2015-12-02 13:57:31 +0100

    namnsdag: add new broken solution

--- onlinekval/namnsdag/submissions/wrong_answer/namnsdag_addremove.py
+++ onlinekval/namnsdag/submissions/wrong_answer/namnsdag_addremove.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
-name = input()
-n = int(input())
+#!/usr/bin/env python2
+name = raw_input()
+n = int(raw_input())
 for i in range(n):
-	dif = sum(1 if x != y else 0 for x, y in zip(input(), name))
+        inp = raw_input()
+	dif = sum(1 if x != y else 0 for x, y in zip(inp, name))
+        print(zip(inp, name))
 	if dif <= 1:
 		print(i+1)
 		break
--- onlinekval/namnsdag/submissions/wrong_answer/namnsdag_addremove2.py
+++ onlinekval/namnsdag/submissions/wrong_answer/namnsdag_addremove2.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python2 
+#!/usr/bin/env python2
 name = raw_input()
 n = int(raw_input())
 for i in range(n):
-	inp = raw_input()
+        inp = raw_input()
 	dif = sum(1 if x != y else 0 for x, y in zip(inp, name))
-	if dif <= 1 and len(inp) == len(name):
+        dif += abs(len(name) - len(inp))
+	if dif <= 1:
 		print(i+1)
 		break
