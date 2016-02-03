7583f406ebd82a96f149ec3bb3e1fca6b6ff739e
Simon Lindholm <simon.lindholm10@gmail.com>
2016-02-03 17:17:13 +0100

    labbplanering: test data

---
+++ final/labbplanering/data/random_generator.py
@@ -0,0 +1,22 @@
+import sys
+import random
+
+case = sys.argv[1]
+pres = int(sys.argv[2])
+s = int(sys.argv[3])
+
+random.seed(s)
+
+def t():
+    return 11 if case == 'same' else random.randint(1, 60)
+
+pr = []
+while pres > 0:
+    npr = 2 if case == 'two' else min(random.choice([1, 2, 2, 3, 4]), pres)
+    pres -= npr
+    pr.append(npr)
+
+print(len(pr))
+for npr in pr:
+    ar = [npr] + [t() for _ in range(npr)]
+    print(' '.join(map(str, ar)))
---
+++ final/labbplanering/graders/grader.py
@@ -0,0 +1,37 @@
+GROUP_SCORES = [30, 20, 10, 20, 20]
+GROUP_CASES = [[3, 4], [0], [1], [0, 1, 3, 4, 5], [0, 1, 2, 3, 4, 5]]
+import sys
+
+def is_int(arg):
+    try:
+        int(arg)
+        return True
+    except:
+        return False
+
+
+def main():
+    if "ignore" in sys.argv:
+        print "AC 0"
+    elif "groups" in sys.argv:
+        scores = []
+        for line in sys.stdin.readlines():
+            verdict, score = line.split()
+            if verdict != "AC":
+                scores.append(0)
+            else:
+                scores.append(score)
+        score = 0
+        for group in range(len(GROUP_SCORES)):
+            group_score = GROUP_SCORES[group]
+            for case in GROUP_CASES[group]:
+                if scores[case] == 0:
+                    group_score = 0
+            score += group_score
+        print "AC %s" % score
+    else:
+        for line in sys.stdin.readlines():
+            print line
+
+main()
+
