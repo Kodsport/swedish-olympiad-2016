b7e34e9321bca0f1cfde3a4f7152ea3c34f5a732
EmanuelGedin <egedin@kth.se>
2016-02-05 17:44:27 +0100

    Fixed solution

--- final/solitarschack/submissions/accepted/solitarschack_ema_random.cpp
+++ final/solitarschack/submissions/accepted/solitarschack_ema_random.cpp
@@ -42,11 +42,14 @@ bool canMove(Piece& a, Piece& b) {
 		    (abs(a.r - b.r) == 0 && abs(a.c - b.c) == a.t) ||
 		    (abs(a.r - b.r) == a.t && abs(a.c - b.c) == a.t));
 	} else if (a.t == 5) {
-		return (a.r == b.r || a.c == b.c);
+		return (a.r == b.r && (b.c == 0 || b.c == 5)) || 
+		       (a.c == b.c && (b.r == 0 || b.r == 5));
 	} else if (a.t == 6) {
-		return (abs(a.r - b.r) == abs(a.c - b.c));
+		return (abs(a.r - b.r) == abs(a.c - b.c)) && (b.r == 0 || b.r == 5 || b.c == 0 || b.c == 5);
 	} else if (a.t == 7) {
-		return (a.r == b.r || a.c == b.c) || (abs(a.r - b.r) == abs(a.c - b.c));
+		return (a.r == b.r && (b.c == 0 || b.c == 5)) || 
+		       (a.c == b.c && (b.r == 0 || b.r == 5)) ||
+		       ((abs(a.r - b.r) == abs(a.c - b.c)) && (b.r == 0 || b.r == 5 || b.c == 0 || b.c == 5));
 	} else if (a.t == 8) {
 	        return (abs(a.r - b.r) == 1 && abs(a.c - b.c) == 2) ||
 		       (abs(a.r - b.r) == 2 && abs(a.c - b.c) == 1);	
