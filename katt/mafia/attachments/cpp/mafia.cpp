3e6ef89def1f5564b590227036bfbb35143655d1
Johan Sannemo <johan.sannemo@gmail.com>
2016-03-11 14:49:42 +0100

    mafia: attachments

---
+++ katt/mafia/attachments/cpp/mafia.cpp
@@ -0,0 +1,7 @@
+#include "mafia.h"
+
+void cops(int N, int M, int A[], int B[], int T[]) {
+}
+
+long long guess(int C) {
+}
---
+++ katt/mafia/attachments/java/mafia.java
@@ -0,0 +1,8 @@
+public class mafia {
+    public static void cops(int N, int M, int[] A, int[] B, int[] C) {
+		}
+
+    public static long guess(int C) {
+      return -1;
+    }
+}
