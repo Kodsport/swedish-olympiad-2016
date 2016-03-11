ca9692f455a2f1f080f5e3ed8bb9a8e1b07805ea
Johan Sannemo <johan.sannemo@gmail.com>
2016-03-11 15:05:01 +0100

    correct name on mafia

--- katt/mafia/include/java/grader.java
+++ katt/mafia/include/java/grader.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class main {
+public class grader {
 
 
   public static void main(String... args) throws IOException {
--- katt/mafia/include/python2/grader.py
+++ katt/mafia/include/python2/grader.py
@@ -5,7 +5,7 @@ N, M = map(int, sys.stdin.readline().split())
 A = [ int(t) for t in sys.stdin.readline().strip().split() ]
 B = [ int(t) for t in sys.stdin.readline().strip().split() ]
 T = [ int(t) for t in sys.stdin.readline().strip().split() ]
-G = int(sys.stdin.readLine())
+G = int(sys.stdin.readline())
 C = [ int(t) for t in sys.stdin.readline().strip().split() ]
 
 mafia.cops(N, M, A, B, T)
